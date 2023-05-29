1ab71d3b45cc8ba90e490d2c742fb248dd5e0d50
Thabo Buthelezi <thabo.buthelezi.tb94@gmail.com>
2023-05-29 15:55:32 +0200

    first commit

---
+++ CocoaTouch/MyButton.swift
@@ -0,0 +1,20 @@
+//
+//  MyButton.swift
+//  CocoaTouch
+//
+//  Created by DA MAC M1 146 on 2023/05/19.
+//
+
+import UIKit
+
+class MyButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
